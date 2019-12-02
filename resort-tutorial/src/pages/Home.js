de2c11ee00e90f4fb0c55f8bbc55f046e6326eee
cv <g.vega.cl@gmail.com>
2019-12-01 22:01:15 -0600

    end of first part, 60min mark

--- resort-tutorial/src/pages/Home.js
+++ resort-tutorial/src/pages/Home.js
@@ -1,10 +1,8 @@
-import React from 'react'
-
+import React from 'react';
+import Hero from '../components/Hero';
 const home = () => {
 	return (
-		<div>
-			hello from home page
-		</div>
+		<Hero></Hero>
 	)
 }
 
